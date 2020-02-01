1aba578b05bb052e182bd8b0abaa25ac6a70be5c
Hyonsoo Han <hansune@me.com>
2020-02-01 15:18:29 +0900

    Initial Commit

---
+++ SwiftUI_study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_study
+//
+//  Created by hanhyonsoo on 2020/02/01.
+//  Copyright © 2020 HSL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
